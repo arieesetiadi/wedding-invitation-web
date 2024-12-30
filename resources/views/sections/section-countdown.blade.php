181a4c191c511e1826df5ed832eda75fe7a64694
Arie Setiadi <arie@eannovate.com>
2024-12-30 16:34:59 +0800

    adjust background image on countdown section

--- resources/views/sections/section-countdown.blade.php
+++ resources/views/sections/section-countdown.blade.php
@@ -1,31 +1,26 @@
 <section id="countdown" class="bg-light">
-    <div class="header mb-5" data-aos="fade-in">
-        <h2 class="text-primary fs-40 fw-700 ff-dancing-script px-lg-0 px-5 text-center">
-            You're Invited
-        </h2>
-    </div>
-
-    <div class="content container" data-aos="fade-in">
-        <div class="d-flex justify-content-center mb-3">
-            <img width="18" height="18" src="{{ asset('assets/images/icons/calendar-black.svg') }}"
-                alt="Calendar icon">
-        </div>
-
-        <span class="d-block text-uppercase fs-18 fw-500 ff-fira-sans mb-2 text-center text-black">
-            Saturday
-        </span>
-
-        <span class="d-block fs-32 fw-400 ff-fira-sans mb-5 text-center text-black">
-            15 February 2025
-        </span>
-
-        <div class="d-flex justify-content-center">
-            <div class="timer-days d-flex align-items-center justify-content-center bg-primary rounded-40">
-                <div class="text-white">
-                    <span class="d-block fs-36 ff-fira-code number mb-1 text-center">
-                        {{-- Generated from script --}}
-                    </span>
-                    <span class="d-block fs-15 ff-fira-sans text-center">Days</span>
+    <div class="wrapper" data-aos="fade-in">
+        <div >
+            <div class="header mb-4" data-aos="fade-in">
+                <h2 class="text-primary fs-40 fw-700 ff-dancing-script px-lg-0 px-5 text-center">
+                    You're Invited
+                </h2>
+            </div>
+        
+            <div class="content container" data-aos="fade-in">
+                <span class="d-block fs-24 fw-400 mb-4 text-center text-black">
+                    <span class="text-uppercase">Saturday, February 15</span><sup>th</sup> 2025
+                </span>
+        
+                <div class="d-flex justify-content-center pt-2">
+                    <div class="timer-days d-flex align-items-center justify-content-center bg-secondary rounded-16">
+                        <div class="text-white">
+                            <span class="d-block fs-36 ff-fira-code number mb-1 text-center">
+                                {{-- Generated from script --}}
+                            </span>
+                            <span class="d-block fs-15 ff-fira-sans text-center">Days</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
